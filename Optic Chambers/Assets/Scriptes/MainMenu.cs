51dc6a8a9e6b3469b55d7834f1918a514bf1eab8
Grégory Grzesiak <gregory@grzesiak.fr>
2023-11-25 23:17:04 +0100

    Edit Main Menu

--- Optic Chambers/Assets/Scriptes/MainMenu.cs
+++ Optic Chambers/Assets/Scriptes/MainMenu.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame(int level)
     {
-        SceneManager.LoadSceneAsync("Level1");
+        SceneManager.LoadSceneAsync($"Level{level}");
     }
-
+    public void GoTemplate()
+    {
+        SceneManager.LoadSceneAsync("Templatelvl");
+    }
     public void QuitGame()
     {
         Application.Quit();
